3c7087beeb549f3d94a095be97425c906f816352
parvk11 <kparv01@gmail.com>
2025-01-08 16:28:08 -0500

    made an app

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Request, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
 from pydantic import BaseModel
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import joblib
-from util import make_user_ratings, get_user_recommendations, predict_ratings_user, show_top
+
+from util import make_user_ratings, get_user_recommendations, predict_ratings_user, show_top, get_password_hash, get_current_user, verify_password, create_access_token, User, UserPreference, get_db, UserToWatch
 from util import X, b, average_ratings, user_w, df2
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+import os
+import logging
+import time
+
+
+SECRET_KEY = '6e8d5097c32ef72dba8a1f17aac1f6da261e489b34432ce4cbbbb85c46be107aa4847ae9317d74e1fa327473fdaf769b00b60d034e48c641c1ff3f0de4f890c7cf35934e8919beefd5106d007dd4076fd85d097e52397c3dd62710fbdb33f1efb5874a98d20be049bd6eb7559ff4b59abc784e50303f537016878dd4c6d90cb3'
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+logging.basicConfig(level=logging.INFO)
+
 
 app = FastAPI()
 
@@ -24,32 +43,157 @@ app.add_middleware(
 async def test():
  return "Hello World!"
 
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+
+    # Log the request details
+    logging.info(f"Incoming request: {request.method} {request.url}")
+    logging.info(f"Headers: {request.headers}")
+    body = await request.body()
+    logging.info(f"Body: {body.decode('utf-8')}")
+
+    # Process the request
+    response = await call_next(request)
+
+    # Log the response details
+    process_time = time.time() - start_time
+    logging.info(f"Response status: {response.status_code}")
+    logging.info(f"Process time: {process_time:.2f} seconds")
+
+    return response
+
+@app.get("/api/movies")
+def get_movies():
+    movies = df2['title'].tolist()
+    # print(movies)
+    return movies
+@app.post("/api/register")
+def register(username: str = Query(...), password: str = Query(...), db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.username == username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    
+    hashed_password = get_password_hash(password)
+    new_user = User(username=username, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {"message": "User created successfully"}
+
+@app.post("/token")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    print("hello")
+    user = db.query(User).filter(User.username == form_data.username).first()
+    print(user)
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": str(user.id)}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.delete("/api/delete")
+def delete_user(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    db.delete(current_user)
+    db.query(UserPreference).filter(UserPreference.user_id == current_user.id).delete()
+    db.commit()
+    return {"message": "User deleted successfully"}
+
+#add movie to watchlist
+@app.post("/api/watchlist/{movie}")
+async def add_movie(movie: str, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    # check if the movie already exists in the user's watchlist
+    existing_preference = db.query(UserToWatch).filter(
+        UserToWatch.user_id == current_user.id,
+        UserToWatch.movie_title == movie
+    ).first()
+    
+    if existing_preference:
+        raise HTTPException(status_code=400, detail="Movie already exists in watchlist")
+    else:
+        new_preference = UserToWatch(user_id=current_user.id, movie_title=movie)
+        db.add(new_preference)
+        try:
+            db.commit()
+        except Exception as e:
+            db.rollback()
+            raise HTTPException(status_code=400, detail="Error saving user preferences") 
+        return {"message": "Movie added successfully"}
+@app.get("/api/watchlist")
+def get_watchlist(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    user_watchlist = db.query(UserToWatch).filter(UserToWatch.user_id == current_user.id).all()
+    watchlist = [preference.movie_title for preference in user_watchlist]
+    return watchlist
+@app.delete("/api/watchlist/{movie}")
+def delete_movie_from_watchlist(movie: str, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    db.query(UserToWatch).filter(UserToWatch.user_id == current_user.id, UserToWatch.movie_title == movie).delete()
+    db.commit()
+    return {"message": "Movie deleted successfully"}
+
 
 @app.post("/api/showmovies")
-async def recommend(ratings : dict):
+async def recommend(ratings : dict, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    #process user ratings
     ratings_from_user = ratings
-
-
     rm_names, rm_inds, rm_mids, us_ratings = make_user_ratings(ratings_from_user)
     my_ratings_normal = us_ratings - average_ratings
 
-
+    #get recommendations
     my_weights = get_user_recommendations(my_ratings_normal, X, b, lam = 1, iterations = 20,user_w = user_w)
-
-
     my_new_predicted = predict_ratings_user(X,my_weights,average_ratings)
 
-
+    #get top recommendations
     my_new_predicted = tf.squeeze(my_new_predicted)
     newix = tf.argsort(my_new_predicted, direction = "DESCENDING")
     best = show_top(topindices= newix, predicted_user_movies= my_new_predicted, df2 = df2, rated_movie_ids = rm_mids)
     best = best.sort_values("My Prediction", ascending = False)
     keys = best["title"].tolist()
+    genres = best["genres"].tolist()
+    rating_user = best["My Prediction"].tolist()
+    keys_genres = []
+    for i in range(len(keys)):
+        keys_genres.append([keys[i],genres[i],rating_user[i]])
 
-    
-    # values = best["My Prediction"].tolist()
-    # new_dict = {}
-    # for key,value in zip(keys, values):
-    #     new_dict[key] = value
-    # return new_dict
-    return JSONResponse(content = best.to_json(orient = 'split'))
+
+
+
+    # Save ratings while avoiding duplicates
+    for movie, rating in ratings.items():
+        # Check if the movie already exists for the current user
+        existing_preference = db.query(UserPreference).filter(
+            UserPreference.user_id == current_user.id,
+            UserPreference.movie_title == movie
+        ).first()
+
+        if existing_preference:
+            # If the movie already exists, update the rating
+            existing_preference.rating = rating
+        else:
+            # If the movie does not exist, create a new entry
+            new_preference = UserPreference(user_id=current_user.id, movie_title=movie, rating=rating)
+            db.add(new_preference)
+    try:
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Error saving user preferences")
+
+    # return JSONResponse(content = best.to_json(orient = 'split'))
+    print(keys_genres[:100])
+    return keys_genres[:100]
+
+@app.get("/api/userprefernces")
+def get_user_preferences(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    user_preferences = db.query(UserPreference).filter(UserPreference.user_id == current_user.id).all()
+    preferences_list = [
+        {"movie_title": preference.movie_title, "rating": preference.rating}
+        for preference in user_preferences
+    ]
+
+    return preferences_list
+@app.delete("/api/userprefernces/{movie}")
+def delete_movie(movie: str, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    db.query(UserPreference).filter(UserPreference.user_id == current_user.id, UserPreference.movie_title == movie).delete()
+    db.commit()
+    return {"message": "Movie deleted successfully"}
--- util.py
+++ util.py
@@ -1,16 +1,67 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
 from pydantic import BaseModel
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import joblib
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+import os
+
+tf.config.run_functions_eagerly(True)
+
+
+SECRET_KEY = '6e8d5097c32ef72dba8a1f17aac1f6da261e489b34432ce4cbbbb85c46be107aa4847ae9317d74e1fa327473fdaf769b00b60d034e48c641c1ff3f0de4f890c7cf35934e8919beefd5106d007dd4076fd85d097e52397c3dd62710fbdb33f1efb5874a98d20be049bd6eb7559ff4b59abc784e50303f537016878dd4c6d90cb3'
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 model = joblib.load("movie-recommender.pkl")
 
-class UserInput(BaseModel):
-    movie_w_rating: dict
+# Password hashing
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Database setup
+DATABASE_URL = "sqlite:///./movie_recommender.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+class UserPreference(Base):
+    __tablename__ = "user_preferences"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    movie_title = Column(String, index=True, nullable=False)
+    rating = Column(Integer, nullable=False)
+class UserToWatch(Base):
+    __tablename__ = "user_to_watch"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    movie_title = Column(String, index=True, nullable=False)
+
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
@@ -36,6 +87,31 @@ num_features = X.shape[1]
 num_movies = X.shape[0]
 user_w = tf.Variable(tf.random.normal((1,  num_features),dtype=tf.float64),  name='user_w')
 optimizer = keras.optimizers.Adam(learning_rate=.1)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def  get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        user = db.query(User).filter(User.id == int(user_id)).first()
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
 
 def show_top(topindices, predicted_user_movies, df2,rated_movie_ids):
     newdf2 = df2.copy()
@@ -137,12 +213,13 @@ def get_user_recommendations(user_ratings, X, b, lam, iterations, user_w):
     user_r = [0]*num_movies
     user_ratings_tensor = tf.convert_to_tensor(user_ratings, dtype=tf.float64)
     user_r = tf.cast(user_ratings_tensor > 0, dtype=tf.float64)
+    print(user_r, user_ratings_tensor)
     for i in range(iterations):
         with tf.GradientTape() as tape:
             error = ((tf.matmul(X,tf.transpose(user_w)))- tf.reshape(user_ratings, (-1,1))) * tf.reshape(user_r, (-1,1))
-            cost = (tf.reduce_sum(tf.square(error)) + (lam/2) *  tf.reduce_sum(tf.square(user_w)))
+            cost = (tf.reduce_sum(tf.square(error)) + (lam/2) *  tf.reduce_sum(tf.square(user_w)))  
         gradients = tape.gradient(cost, [user_w])
         optimizer.apply_gradients(zip(gradients, [user_w]))
         if(i % 10 == 0):
-            print(f'Cost at epoch {i}: {cost}')
+            print(f'Cost at epoch {i}: {cost.numpy()}')
     return user_w
